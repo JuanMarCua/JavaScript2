b8e2586d265b6b552bbeeaee4c5de939def1ed86
JuanMarCua <juanmarcua@gmail.com>
2022-02-25 17:43:49 +0100

    Text inclusion v0.1

--- FamousQuotes/scriptSelector.js
+++ FamousQuotes/scriptSelector.js
@@ -1,9 +1,43 @@
+// Get text input
+
+let wordAsked="";
+const getVal = () => {
+    wordAsked = document.querySelector('wordAsked').innerHTML;
+
+}
+
 window.onload = function(){
-    //Search of author and quote 
-    document.getElementById('author').innerHTML = famousQuotes[1].quoteAuthor;
-    document.getElementById('quote').innerHTML = famousQuotes[1].quoteText;
+    //Search of author and quote: 
+    //Select searched word (pend introduce in HTML):
+    //This function will return the ID of one of the random quotes that has the asked word
+    const lookForWord = (wordIn, famousQuotesIn) => {
+        let idPos = 0;
+        let arrRightQuotes = [];
+        console.log("Enters function");
+
+        for (let element of famousQuotesIn) {
+            if (element.quoteText.includes(wordIn)) {
+                arrRightQuotes.push(famousQuotesIn.indexOf(element));
+            }
+        }
+        console.log("arrRightQuotes = " + arrRightQuotes);
+        randId = Math.floor(Math.random() * arrRightQuotes.length);
+        idPos = arrRightQuotes[randId];
+        return idPos;
+    };
 
     //Random selection of Author and Quote
     let  randPos = Math.floor(Math.random() * famousQuotes.length);
-    document.getElementById('randQuote').innerHTML = famousQuotes[randPos].quoteText + " -" + famousQuotes[randPos].quoteAuthor;
+    if (famousQuotes[randPos].quoteAuthor == "") {
+        famousQuotes[randPos].quoteAuthor = "Unknown";
+    }
+    document.getElementById('randQuote').innerHTML = famousQuotes[randPos].quoteText + " - " + famousQuotes[randPos].quoteAuthor;
+
+    //Author and quote presentation:
+    idDef = lookForWord(wordAsked, famousQuotes);
+    if (famousQuotes[idDef].quoteAuthor == "") {
+        famousQuotes[idDef].quoteAuthor = "Unknown";
+    }
+    document.getElementById('author').innerHTML = famousQuotes[idDef].quoteAuthor;
+    document.getElementById('quote').innerHTML = famousQuotes[idDef].quoteText;
 };
