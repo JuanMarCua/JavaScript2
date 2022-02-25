269bf026ad48ecbd8faba90bd41e2ab83ad43c5d
JuanMarCua <juanmarcua@gmail.com>
2022-02-25 12:40:32 +0100

    correlation between javascript and html

---
+++ FamousQuotes/scriptSelector.js
@@ -0,0 +1,4 @@
+window.onload = function(){
+    document.getElementById('author').innerHTML = famousQuotes[1].quoteAuthor;
+    document.getElementById('quote').innerHTML = famousQuotes[1].quoteText;
+};
