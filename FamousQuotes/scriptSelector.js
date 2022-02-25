d9a100e733fa717e4bea5994b1f95feef4153d82
JuanMarCua <juanmarcua@gmail.com>
2022-02-25 13:55:35 +0100

    Solved Random Header

--- FamousQuotes/scriptSelector.js
+++ FamousQuotes/scriptSelector.js
@@ -1,4 +1,9 @@
 window.onload = function(){
+    //Search of author and quote 
     document.getElementById('author').innerHTML = famousQuotes[1].quoteAuthor;
     document.getElementById('quote').innerHTML = famousQuotes[1].quoteText;
+
+    //Random selection of Author and Quote
+    let  randPos = Math.floor(Math.random() * famousQuotes.length);
+    document.getElementById('randQuote').innerHTML = famousQuotes[randPos].quoteText + " -" + famousQuotes[randPos].quoteAuthor;
 };
